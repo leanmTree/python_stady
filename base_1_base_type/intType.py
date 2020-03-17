06c88d4655008671ddb78907ffaf5c4fc03c9a43
吴恒涛 <hengtao.wu@teiimwork.com>
2020-03-17 09:28:29 +0800

    this is marge

--- base_1_base_type/intType.py
+++ base_1_base_type/intType.py
@@ -1,11 +1,23 @@
-import turtle
-import math
+# import turtle
+# import math
+#
+# x = y = 100
+#
+# turtle.forward(100)
+# turtle.forward(-200)
+# turtle.goto(0, 0)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.forward(-200)
 
-x = y = 100
+# 变量赋值
+# a = 123
+# print(id(a)) # 获取变量a的内存地址
+# b = 123
+# print(id(b)) # 此时b的内存指向的也是123堆内存的地址
+# del a # 将变量a从栈内存中删除
 
-turtle.forward(100)
-turtle.forward(-200)
-turtle.goto(0, 0)
-turtle.left(90)
-turtle.forward(100)
-turtle.forward(-200)
+
+# 整数转型,并且整数在python3中不限制大小，永远不会溢出
+# price = '123'
+# print(int(price))
